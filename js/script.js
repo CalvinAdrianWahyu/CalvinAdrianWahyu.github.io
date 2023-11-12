e20f5cbc628f49aee64ce4b861ddea450783904c
Fandy Fathurrohman <135007919+Fandyfr@users.noreply.github.com>
2023-11-13 01:49:03 +0800

    Update Web

--- js/script.js
+++
@@ -1,7 +0,0 @@
- var typed = new typed(".multiple-text",{
-     strings: ["Game Developer", "Hacker", "Ahli Logika", "Web Developer"],
-     typespeed: 150,
-     backspeed: 150,
-     backdelay: 1000,
-     loop: true   
-})
